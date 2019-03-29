66f70d03d13235250b5ee28d5231cc2155bd5d55
dannydarku <dannydarku@gmail.com>
2019-03-29 00:53:09 -0400

    adding root to route

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   resources :comments
   get 'pages/info'
   resources :ideas
+  
+  
+  root to: 'ideas#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
